1650d505d24e717a313a9d1d7d17162e9bf554fe
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-03-30 15:57:45 +0530

    added sarga tutorial to be able to improve grammar

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import { sketch } from './sketch';
 import test0 from 'raw-loader!../samples/test0.sarga';
 import test1 from 'raw-loader!../samples/test1.sarga';
 import test2 from 'raw-loader!../samples/test2.sarga';
+import sargatut from 'raw-loader!../samples/sargatut.sarga';
 import { Sarga, SargaSemantics } from './sarga_main';
 
 // const sketchInstance = new p5(sketch);
 
 const examples = [
-    test0,
-    test1,
-    test2
+    // test0,
+    // test1,
+    // test2
+    sargatut
 ];
 
 for (let eg of examples) {
