67929590a1451db7a7d4e8723fd7780c037805e2
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-04-02 14:49:47 +0530

    entity construction properties are optional

--- src/sarga_operations.js
+++ src/sarga_operations.js
@@ -109,9 +109,14 @@ export const evalOperation = {
     },
 
     VariableDeclaration(varName, isAKW, type, properties) {
+        let props = properties.eval();
+        let propsArr = [];
+        if(props && props.length > 0) {
+            propsArr = props[0];
+        }
         return {
             type: "declaration",
-            statement: ["var", varName.sourceString, type.sourceString, properties.eval()]
+            statement: ["var", varName.sourceString, type.sourceString, propsArr]
         };
     },
 
